d3efa114e5ea8dd0cab13f51e4ccef2febf158e9
TomL94 <langer.tom7@gmail.com>
2020-02-02 17:35:03 +0200

    Added internal types for enigmachain and changed constants for the addresses prefixes

---
+++ types/alias.go
@@ -0,0 +1,15 @@
+package types
+
+import (
+	"github.com/enigmampc/Enigmachain/types/util"
+)
+
+// Aliases for internal types
+const (
+	Bech32PrefixAccAddr  = util.Bech32PrefixAccAddr
+	Bech32PrefixAccPub   = util.Bech32PrefixAccPub
+	Bech32PrefixValAddr  = util.Bech32PrefixValAddr
+	Bech32PrefixValPub   = util.Bech32PrefixValPub
+	Bech32PrefixConsAddr = util.Bech32PrefixConsAddr
+	Bech32PrefixConsPub  = util.Bech32PrefixConsPub
+)
---
+++ types/util/prefix.go
@@ -0,0 +1,16 @@
+package util
+
+const (
+	// Bech32PrefixAccAddr defines the Bech32 prefix of an account's address
+	Bech32PrefixAccAddr = "enigma"
+	// Bech32PrefixAccPub defines the Bech32 prefix of an account's public key
+	Bech32PrefixAccPub = "enigmapub"
+	// Bech32PrefixValAddr defines the Bech32 prefix of a validator's operator address
+	Bech32PrefixValAddr = "enigmavaloper"
+	// Bech32PrefixValPub defines the Bech32 prefix of a validator's operator public key
+	Bech32PrefixValPub = "enigmavaloperpub"
+	// Bech32PrefixConsAddr defines the Bech32 prefix of a consensus node address
+	Bech32PrefixConsAddr = "enigmavalcons"
+	// Bech32PrefixConsPub defines the Bech32 prefix of a consensus node public key
+	Bech32PrefixConsPub = "enigmavalconspub"
+)
