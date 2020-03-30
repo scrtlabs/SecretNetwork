f025097257535bf4fd91ee6c91cf76c7011ba94a
assafmo <assaf.morami@gmail.com>
2020-03-30 11:31:31 +0300

    contract_operations.rs comments

--- cosmwasm/lib/wasmi-runtime/src/contract_operations.rs
+++ cosmwasm/lib/wasmi-runtime/src/contract_operations.rs
@@ -6,13 +6,6 @@ use super::imports;
 use super::results::{HandleSuccess, InitSuccess, QuerySuccess};
 use crate::exports;
 
-/// This is a safe wrapper for allocating buffers outside the enclave.
-pub(super) fn allocate_user_space_buffer(buffer: &[u8]) -> UserSpaceBuffer {
-    let ptr = buffer.as_ptr();
-    let len = buffer.len();
-    unsafe { imports::ocall_allocate(ptr, len) }
-}
-
 /// Safe wrapper around reads from the contract storage
 fn read_db(context: Ctx, key: &[u8]) -> Option<Vec<u8>> {
     unsafe { exports::recover_buffer(imports::ocall_read_db(context, key.as_ptr(), key.len())) }
@@ -31,13 +24,20 @@ fn write_db(context: Ctx, key: &[u8], value: &[u8]) {
     }
 }
 
+// These functions 
+// fn read_db(key: *const c_void, value: *mut c_void) -> i32;
+// fn write_db(key: *const c_void, value: *mut c_void);
+// fn canonicalize_address(human: *const c_void, canonical: *mut c_void) -> i32;
+// fn humanize_address(canonical: *const c_void, human: *mut c_void) -> i32;
+
 pub fn init(
-    context: Ctx,
-    contract: &[u8],
-    env: &[u8],
-    msg: &[u8],
+    context: Ctx,    // need to pass this to read_db & write_db
+    contract: &[u8], // contract wasm bytes
+    env: &[u8],      // blockchain state
+    msg: &[u8],      // probably function call and args
 ) -> Result<InitSuccess, EnclaveError> {
     todo!()
+    // init wasmi
 }
 
 pub fn handle(
@@ -47,8 +47,11 @@ pub fn handle(
     msg: &[u8],
 ) -> Result<HandleSuccess, EnclaveError> {
     todo!()
+    // init wasmi - maybe the same as init for now?
 }
 
 pub fn query(context: Ctx, contract: &[u8], msg: &[u8]) -> Result<QuerySuccess, EnclaveError> {
     todo!()
+    // init wasmi
+    // no access to write_db
 }
