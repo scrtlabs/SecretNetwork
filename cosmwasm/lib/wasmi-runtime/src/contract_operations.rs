46b89f00c6578f28b3e9ae0c3fecb8ab717eff7b
assafmo <assaf.morami@gmail.com>
2020-04-02 16:12:08 +0300

    ...

--- cosmwasm/lib/wasmi-runtime/src/contract_operations.rs
+++ cosmwasm/lib/wasmi-runtime/src/contract_operations.rs
@@ -4,17 +4,17 @@ use super::imports;
 use super::results::{HandleSuccess, InitSuccess, QuerySuccess};
 use crate::exports;
 
+use std::borrow::ToOwned;
+use std::cell::RefCell;
 use std::ptr;
 use std::slice;
 
 extern crate wasmi;
 use wasmi::{
-    Error as InterpreterError, Externals, FuncInstance, FuncRef, HostError, ImportsBuilder,
-    ModuleImportResolver, ModuleInstance, ModuleRef, RuntimeArgs, RuntimeValue, Signature, Trap,
-    ValueType,
+    memory_units, Error as InterpreterError, Externals, FuncInstance, FuncRef, ImportsBuilder,
+    MemoryDescriptor, MemoryInstance, MemoryRef, ModuleImportResolver, ModuleInstance, RuntimeArgs,
+    RuntimeValue, Signature, Trap, ValueType,
 };
-
-use sp_wasm_interface::Pointer;
 
 /// Safe wrapper around reads from the contract storage
 fn read_db(context: Ctx, key: &[u8]) -> Option<Vec<u8>> {
@@ -62,7 +62,8 @@ pub fn init(
 
     // Create new imports resolver.
     // These are the signatures of rust functions available to invoke from wasm code.
-    let imports = ImportsBuilder::new().with_resolver("env", &EnigmaImportResolver);
+    let enigma_import_resolver = EnigmaImportResolver::with_limit(4 * 1024 * 1024 * 1024); // 4GiB
+    let imports = ImportsBuilder::new().with_resolver("env", &enigma_import_resolver);
 
     // Instantiate a module with our imports and assert that there is no `start` function.
     let not_started_instance =
@@ -136,7 +137,40 @@ pub fn query(context: Ctx, contract: &[u8], msg: &[u8]) -> Result<QuerySuccess, 
 // EnigmaImportResolver maps function name to its function signature and also to function index in Runtime
 // When instansiating a module we give it this resolver
 // When invoking a function inside the module we can give it different runtimes (which we probably won't do)
-struct EnigmaImportResolver;
+#[derive(Debug, Clone)]
+pub struct EnigmaImportResolver {
+    max_memory: u32,
+    memory: RefCell<MemoryRef>,
+}
+
+impl EnigmaImportResolver {
+    /// New import resolver with specifed maximum amount of inital memory (in wasm pages = 64kb)
+    pub fn with_limit(max_memory: u32) -> EnigmaImportResolver {
+        EnigmaImportResolver {
+            max_memory,
+            memory: RefCell::new(
+                MemoryInstance::alloc(
+                    memory_units::Pages(0),
+                    Some(memory_units::Pages(max_memory as usize)),
+                )
+                .expect("Reuven to fix this"),
+            ),
+        }
+    }
+
+    /// Returns memory that was instantiated during the contract module
+    /// start. If contract does not use memory at all, the dummy memory of length (0, 0)
+    /// will be created instead. So this method always returns memory instance
+    /// unless errored.
+    pub fn memory_ref(&self) -> MemoryRef {
+        self.memory.borrow().clone()
+    }
+
+    /// Returns current memory that is in use
+    pub fn memory_size(&self) -> Result<u32, InterpreterError> {
+        Ok(self.memory_ref().current_size().0 as u32)
+    }
+}
 
 // These functions should be available to invoke from wasm code
 // These should pass the request up to go-cosmwasm:
@@ -146,6 +180,34 @@ struct EnigmaImportResolver;
 // fn canonicalize_address(human: *const c_void, canonical: *mut c_void) -> i32;
 // fn humanize_address(canonical: *const c_void, human: *mut c_void) -> i32;
 impl ModuleImportResolver for EnigmaImportResolver {
+    fn resolve_memory(
+        &self,
+        field_name: &str,
+        descriptor: &MemoryDescriptor,
+    ) -> Result<MemoryRef, InterpreterError> {
+        if field_name == "memory" {
+            let effective_max = descriptor.maximum().unwrap_or(self.max_memory + 1);
+            if descriptor.initial() > self.max_memory || effective_max > self.max_memory {
+                Err(InterpreterError::Instantiation(
+                    "Module requested too much memory".to_owned(),
+                ))
+            } else {
+                let mem = MemoryInstance::alloc(
+                    memory_units::Pages(descriptor.initial() as usize),
+                    descriptor
+                        .maximum()
+                        .map(|x| memory_units::Pages(x as usize)),
+                )?;
+                *self.memory.borrow_mut() = mem.clone();
+                Ok(mem)
+            }
+        } else {
+            Err(InterpreterError::Instantiation(
+                "Memory imported under unknown name".to_owned(),
+            ))
+        }
+    }
+
     fn resolve_func(
         &self,
         func_name: &str,
