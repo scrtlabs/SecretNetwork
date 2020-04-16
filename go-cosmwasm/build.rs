ce86d45678915922e4c9533edc241ffca3838c1e
assafmo <assaf.morami@gmail.com>
2020-04-16 11:51:15 +0300

    fix build in SGX_MODE=SW

--- go-cosmwasm/build.rs
+++ go-cosmwasm/build.rs
@@ -9,6 +9,10 @@ fn main() {
 
     println!("cargo:rustc-link-search=native=./lib");
     println!("cargo:rustc-link-lib=static=Enclave_u");
+
+    let sdk_dir = env::var("SGX_SDK").unwrap_or_else(|_| "/opt/intel/sgxsdk".to_string());
+    println!("cargo:rustc-link-search=native={}/lib64", sdk_dir);
+
     let is_sim = env::var("SGX_MODE").unwrap_or_else(|_| "HW".to_string());
     match is_sim.as_ref() {
         "SW" => println!("cargo:rustc-link-lib=dylib=sgx_urts_sim"),
