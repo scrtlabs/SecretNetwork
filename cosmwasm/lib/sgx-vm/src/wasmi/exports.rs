1217a1c2448ad5ee24858fe704abb3abb6c5e155
TomL94 <langer.tom7@gmail.com>
2020-04-21 14:30:35 +0300

    Added logging functionality in go-cosmwasm and logs for some ocalls

--- cosmwasm/lib/sgx-vm/src/wasmi/exports.rs
+++ cosmwasm/lib/sgx-vm/src/wasmi/exports.rs
@@ -1,12 +1,17 @@
-use std::ffi::c_void;
-
 use enclave_ffi_types::{Ctx, EnclaveBuffer, UserSpaceBuffer};
+use log::info;
+use std::ffi::c_void;
 
 use crate::context::with_storage_from_context;
 
 /// Copy a buffer from the enclave memory space, and return an opaque pointer to it.
 #[no_mangle]
 pub extern "C" fn ocall_allocate(buffer: *const u8, length: usize) -> UserSpaceBuffer {
+    info!(
+        target: module_path!(),
+        "ocall_allocate() called with buffer length: {:?}", length
+    );
+
     let slice = unsafe { std::slice::from_raw_parts(buffer, length) };
     let vector_copy = slice.to_vec();
     let boxed_vector = Box::new(vector_copy);
@@ -30,6 +35,13 @@ pub unsafe fn recover_buffer(ptr: UserSpaceBuffer) -> Option<Vec<u8>> {
 #[no_mangle]
 pub extern "C" fn ocall_read_db(mut context: Ctx, key: *const u8, key_len: usize) -> EnclaveBuffer {
     let key = unsafe { std::slice::from_raw_parts(key, key_len) };
+
+    info!(
+        target: module_path!(),
+        "ocall_read_db() called with len: {:?} key: {:?}",
+        key_len,
+        String::from_utf8_lossy(key)
+    );
     let null_buffer = EnclaveBuffer {
         ptr: std::ptr::null_mut(),
     };
@@ -67,6 +79,15 @@ pub extern "C" fn ocall_write_db(
 ) {
     let key = unsafe { std::slice::from_raw_parts(key, key_len) };
     let value = unsafe { std::slice::from_raw_parts(value, value_len) };
+
+    info!(
+        target: module_path!(),
+        "ocall_write_db() called with key_len: {:?} key: {:?} val_len: {:?} val: {:?}...",
+        key_len,
+        String::from_utf8_lossy(key),
+        value_len,
+        String::from_utf8_lossy(value.get(0..std::cmp::min(20, value_len)).unwrap())
+    );
 
     // We explicitly ignore this potential panic here because we have no way of handling it at the moment.
     // In the future, if we see that panics do occur here, we should add a way to report this to the enclave.
