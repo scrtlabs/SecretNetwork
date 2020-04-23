0227aca35446223e9a701254290c28d9b2236222
assafmo <assaf.morami@gmail.com>
2020-04-23 16:12:49 +0300

    fix gas expectation in test

--- x/compute/internal/keeper/keeper_test.go
+++ x/compute/internal/keeper/keeper_test.go
@@ -253,7 +253,7 @@ func TestExecute(t *testing.T) {
 
 	// make sure gas is properly deducted from ctx
 	gasAfter := ctx.GasMeter().GasConsumed()
-	require.Equal(t, uint64(31162), gasAfter-gasBefore)
+	require.Equal(t, uint64(31325), gasAfter-gasBefore)
 
 	// ensure bob now exists and got both payments released
 	bobAcct = accKeeper.GetAccount(ctx, bob)
