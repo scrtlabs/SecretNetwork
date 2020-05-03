e015e3a3c47d62ebab69a6da03636d301aeb1b73
TomL94 <langer.tom7@gmail.com>
2020-05-03 12:03:55 +0300

    Added CryptoErrors to enclave-ffi-types

--- cosmwasm/lib/enclave-ffi-types/src/lib.rs
+++ cosmwasm/lib/enclave-ffi-types/src/lib.rs
@@ -3,6 +3,6 @@
 mod types;
 
 pub use types::{
-    Ctx, EnclaveBuffer, EnclaveError, HandleResult, InitResult, KeyGenResult, QueryResult,
-    UserSpaceBuffer,
+    CryptoError, Ctx, EnclaveBuffer, EnclaveError, HandleResult, InitResult, KeyGenResult,
+    QueryResult, UserSpaceBuffer,
 };
--- cosmwasm/lib/enclave-ffi-types/src/types.rs
+++ cosmwasm/lib/enclave-ffi-types/src/types.rs
@@ -1,5 +1,6 @@
 use core::ffi::c_void;
 use derive_more::Display;
+use secp256k1::Error;
 
 /// This type represents an opaque pointer to a memory address in normal user space.
 #[repr(C)]
@@ -54,6 +55,63 @@ pub enum EnclaveError {
     OutOfGas,
     /// Unexpected Error happened, no more details available
     Unknown,
+}
+
+pub enum CryptoError {
+    /// The `DerivingKeyError` error.
+    ///
+    /// This error means that the ECDH process failed.
+    DerivingKeyError {
+        self_key: [u8; 64],
+        other_key: [u8; 64],
+    },
+    /// The `MissingKeyError` error.
+    ///
+    /// This error means that a key was missing.
+    MissingKeyError { key_type: &'static str },
+    /// The `DecryptionError` error.
+    ///
+    /// This error means that the symmetric decryption has failed for some reason.
+    DecryptionError,
+    /// The `ImproperEncryption` error.
+    ///
+    /// This error means that the ciphertext provided was imporper.
+    /// e.g. MAC wasn't valid, missing IV etc.
+    ImproperEncryption,
+    /// The `EncryptionError` error.
+    ///
+    /// This error means that the symmetric encryption has failed for some reason.
+    EncryptionError,
+    /// The `SigningError` error.
+    ///
+    /// This error means that the signing process has failed for some reason.
+    SigningError { hashed_msg: [u8; 32] },
+    /// The `ParsingError` error.
+    ///
+    /// This error means that the signature couldn't be parsed correctly.
+    ParsingError { sig: [u8; 65] },
+    /// The `RecoveryError` error.
+    ///
+    /// This error means that the public key can't be recovered from that message & signature.
+    RecoveryError { sig: [u8; 65] },
+    /// The `KeyError` error.
+    ///
+    /// This error means that a key wasn't vaild.
+    /// e.g. PrivateKey, PubliKey, SharedSecret.
+    // #[cfg(feature = "asymmetric")]
+    KeyError {
+        key_type: &'static str,
+        err: Option<secp256k1::Error>,
+    },
+    // #[cfg(not(feature = "asymmetric"))]
+    // KeyError { key_type: &'static str, err: Option<()> },
+    // /// The `RandomError` error.
+    // ///
+    // /// This error means that the random function had failed generating randomness.
+    // #[cfg(feature = "std")]
+    // RandomError { err: rand_std::Error },
+    // #[cfg(feature = "sgx")]
+    // RandomError { err: sgx_types::sgx_status_t },
 }
 
 /// This struct is returned from ecall_init.
