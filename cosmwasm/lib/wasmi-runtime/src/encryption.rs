1dc2c9c07da3f48fd445c4e704820320dda77049
assafmo <assaf.morami@gmail.com>
2020-05-10 17:58:28 +0300

    compiles

--- cosmwasm/lib/wasmi-runtime/src/encryption.rs
+++ cosmwasm/lib/wasmi-runtime/src/encryption.rs
@@ -1,6 +1,6 @@
 use crate::keys::SymmetricKey;
 use enclave_ffi_types::CryptoError;
-use ring::aead::{self, Aad, Nonce};
+use ring::aead::{self, Aad, LessSafeKey, Nonce, UnboundKey};
 use sgx_trts::trts::rsgx_read_rand;
 
 static AES_MODE: &aead::Algorithm = &aead::AES_256_GCM;
@@ -12,27 +12,30 @@ type IV = [u8; IV_SIZE];
 
 /// This function will encrypt a plaintext message and append the tag
 /// The cyphertext can be decrypted with the `decrypt` function below.
-fn encrypt(message: &[u8], key: &SymmetricKey) -> Result<Vec<u8>, CryptoError> {
-    let key = aead::SealingKey::new(&AES_MODE, key).map_err(|_| CryptoError::KeyError {})?;
+fn encrypt(plaintext: &[u8], key: &SymmetricKey) -> Result<Vec<u8>, CryptoError> {
+    let key =
+        LessSafeKey::new(UnboundKey::new(&AES_MODE, key).map_err(|_| CryptoError::KeyError {})?);
 
-    let mut in_out = message.to_owned();
-    let tag_size = AES_MODE.tag_len(); // authenticated tag (includes the IV)
-    in_out.extend(vec![0u8; tag_size]);
-    let seal_size = aead::seal_in_place_append_tag(&key, Aad::empty(), &mut in_out)
+    let mut in_out_buffer = plaintext.to_owned();
+    let nonce = Nonce::assume_unique_for_key([0_u8; 12]); // TODO fix
+
+    key.seal_in_place_append_tag(nonce, Aad::empty(), &mut in_out_buffer)
         .map_err(|_| CryptoError::EncryptionError)?;
 
-    in_out.truncate(seal_size);
-    Ok(in_out)
+    Ok(in_out_buffer.to_vec())
 }
 
 /// This function will decrypt a cipher text only if it was encrypted with the `encrypt` function above.
 /// (data must be encrypted with `seal_in_place_append_tag`)
 pub fn decrypt(ciphertext: &[u8], key: &SymmetricKey) -> Result<Vec<u8>, CryptoError> {
-    let key = aead::OpeningKey::new(&AES_MODE, key).map_err(|_| CryptoError::KeyError {})?;
+    let key =
+        LessSafeKey::new(UnboundKey::new(&AES_MODE, key).map_err(|_| CryptoError::KeyError {})?);
 
     let mut ciphertext = ciphertext.to_owned();
+    let nonce = Nonce::assume_unique_for_key([0_u8; 12]); // TODO fix
+
     let plaintext = key
-        .open_in_place(Aad::empty(), &mut ciphertext)
+        .open_in_place(nonce, Aad::empty(), &mut ciphertext)
         .map_err(|_| CryptoError::DecryptionError)?;
 
     Ok(plaintext.to_vec())
