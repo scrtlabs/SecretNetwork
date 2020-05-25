e2a07741baf06284cb44df81e2134536fc97e088
Itzik Grossman <itzik@keytango.io>
2020-05-25 15:39:49 +0300

    Trying to fix github tests

--- cosmwasm/lib/wasmi-sgx-test.sh
+++ cosmwasm/lib/wasmi-sgx-test.sh
@@ -26,6 +26,8 @@ perl -i -pe 's/"stake"/"uscrt"/g' ~/.enigmad/config/genesis.json
 ./enigmad collect-gentxs
 ./enigmad validate-genesis
 
+find /home/runner/ -name "*.der"
+
 ./enigmad init-bootstrap wasmi-runtime/attestation_cert.der
 ./enigmad validate-genesis
 
