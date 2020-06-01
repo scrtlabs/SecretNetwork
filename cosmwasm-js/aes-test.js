4c6c678fe87c792db2bf4b1a938f264eebb1671b
assafmo <assaf.morami@gmail.com>
2020-06-01 17:33:14 +0300

    siv

--- cosmwasm-js/aes-test.js
+++ cosmwasm-js/aes-test.js
@@ -1,24 +1,11 @@
-const forge = require("node-forge");
+(async () => {
+  const miscreant = require("miscreant");
+  const softProvider = new miscreant.PolyfillCryptoProvider();
+  const key = await miscreant.SIV.importKey(new Uint8Array(new Array(32).fill(0x7)), "AES-SIV", softProvider);
 
-let key = forge.util.createBuffer(new Uint8Array(new Array(32).fill(0x7)).buffer);
-let iv = forge.util.createBuffer(new Uint8Array(new Array(12).fill(0x0)).buffer);
+  const plaintext = new TextEncoder("utf-8").encode("banana");
+  const ciphertext = await siv.seal(plaintext);
 
-const input = forge.util.createBuffer();
-input.putString("banana");
-
-var cipher = forge.cipher.createCipher("AES-GCM", key);
-cipher.start({ iv: iv });
-cipher.update(input);
-cipher.finish();
-var encrypted = cipher.output;
-// outputs encrypted hex
-console.log(encrypted.putBuffer(cipher.mode.tag));
-
-key = forge.util.createBuffer(new Uint8Array(new Array(32).fill(0x7)).buffer);
-iv = forge.util.createBuffer(new Uint8Array(new Array(12).fill(0x0)).buffer);
-
-var decipher = forge.cipher.createDecipher("AES-GCM", key);
-decipher.start({ iv: iv, tag: cipher.mode.tag });
-decipher.update(cipher.output);
-var result = decipher.finish();
-console.log(decipher.output.data);
+  const decrypted = await key.open(ciphertext);
+  console.log(decrypted);
+})();
