a6f97c86a2614008f9a4b643621b43c794c2df85
assafmo <assaf.morami@gmail.com>
2020-06-02 13:48:49 +0300

    siv

--- cosmwasm-js/aes-test.js
+++ cosmwasm-js/aes-test.js
@@ -1,11 +1,14 @@
 (async () => {
   const miscreant = require("miscreant");
   const softProvider = new miscreant.PolyfillCryptoProvider();
-  const key = await miscreant.SIV.importKey(new Uint8Array(new Array(32).fill(0x7)), "AES-SIV", softProvider);
 
-  const plaintext = new TextEncoder("utf-8").encode("banana");
-  const ciphertext = await siv.seal(plaintext);
+  const key = new Uint8Array(new Array(32).fill(0x7));
+  const siv = await miscreant.SIV.importKey(key, "AES-SIV", softProvider);
 
-  const decrypted = await key.open(ciphertext);
-  console.log(decrypted);
+  const plaintext = new Uint8Array([1, 2, 3, 4, 5, 6]);
+  const sealed = await siv.seal(plaintext, []);
+  console.log(sealed);
+
+  const unsealed = await siv.open(sealed, []);
+  console.log(unsealed);
 })();
