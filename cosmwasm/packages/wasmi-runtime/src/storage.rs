fd7c95f7b52ae4b1bbacf323ca9672f89ceaaf38
Reuven Podmazo <reuven@enigma.co>
2020-06-24 18:34:14 +0300

    fixed issue in write_to_untrusted where failure to write would be ignored

--- cosmwasm/packages/wasmi-runtime/src/storage.rs
+++ cosmwasm/packages/wasmi-runtime/src/storage.rs
@@ -4,15 +4,9 @@ use sgx_types::*;
 use std::io::Write;
 use std::untrusted::fs::File;
 
-pub fn write_to_untrusted(bytes: &[u8], filepath: &str) -> SgxResult<sgx_status_t> {
-    File::create(filepath)
-        .map(|f| _write(bytes, f))
-        .sgx_error_with_log(&format!("Creating file '{}' failed", filepath))?
-}
-
-fn _write<F: Write>(bytes: &[u8], mut file: F) -> SgxResult<sgx_status_t> {
-    file.write_all(bytes)
-        .sgx_error_with_log("[Enclave] Writing File failed!")?;
-
-    Ok(sgx_status_t::SGX_SUCCESS)
+pub fn write_to_untrusted(bytes: &[u8], filepath: &str) -> SgxResult<()> {
+    let mut f = File::create(filepath)
+        .sgx_error_with_log(&format!("Creating file '{}' failed", filepath))?;
+    f.write_all(bytes)
+        .sgx_error_with_log("[Enclave] Writing File failed!")
 }
