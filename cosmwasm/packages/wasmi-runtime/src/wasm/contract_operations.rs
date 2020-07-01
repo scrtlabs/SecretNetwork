4cf9a36cd57ecbd6ad55794604c859de24db9037
assafmo <assaf.morami@gmail.com>
2020-07-01 21:30:33 +0300

    fix memory initialization for WASM + debug log errors
    now instead of lowering the min, we fail if the min value requested is bigger than our allowed max

--- cosmwasm/packages/wasmi-runtime/src/wasm/contract_operations.rs
+++ cosmwasm/packages/wasmi-runtime/src/wasm/contract_operations.rs
@@ -218,24 +218,55 @@ fn start_engine(
     let mut p_modlue: Module =
         elements::deserialize_buffer(contract).map_err(|_| EnclaveError::InvalidWasm)?;
 
+    trace!("Deserialized Wasm contract");
+
     let memory_section = p_modlue
         .memory_section_mut()
-        .ok_or(EnclaveError::CannotInitializeWasmMemory)?;
+        .ok_or(EnclaveError::CannotInitializeWasmMemory).map_err(|err| {
+            error!(
+                "Error accessing memory section of WASM while trying to validate memory demands: {:?}",
+                err
+            );
+            err
+        })?;
 
     if memory_section.entries().len() != 1 {
+        error!(
+            "WASM demands too many memory sections. Must be 1, demands {}",
+            memory_section.entries().len()
+        );
         return Err(EnclaveError::CannotInitializeWasmMemory);
     }
 
     let memory_entry = memory_section
         .entries()
         .first()
-        .ok_or(EnclaveError::CannotInitializeWasmMemory)?;
+        .ok_or(EnclaveError::CannotInitializeWasmMemory)
+        .map_err(|err| {
+            error!(
+                "Error accessing memory entry of WASM while trying to validate memory demands: {:?}",
+                err
+            );
+            err
+        })?;
 
-    let maximum: u32 = 192; // 12 MiB
-    let minimum: u32 = std::cmp::min(maximum, memory_entry.limits().initial());
-    *memory_section.entries_mut() = vec![MemoryType::new(minimum, Some(maximum))];
+    let requested_initial_pages: u32 = memory_entry.limits().initial();
+    let maximum_allowed_pages: u32 = 192; // 12 MiB
 
-    trace!("Deserialized Wasm contract");
+    if requested_initial_pages > maximum_allowed_pages {
+        error!(
+            "WASM Requested to initialize with {} pages, maximum allowed is {}",
+            requested_initial_pages, maximum_allowed_pages
+        );
+        return Err(EnclaveError::CannotInitializeWasmMemory);
+    }
+
+    *memory_section.entries_mut() = vec![MemoryType::new(
+        requested_initial_pages,
+        Some(maximum_allowed_pages),
+    )];
+
+    trace!("Validated WASM memory demands");
 
     // Set the gas costs for wasm op-codes (there is an inline stack_height limit in WasmCosts)
     let wasm_costs = WasmCosts::default();
