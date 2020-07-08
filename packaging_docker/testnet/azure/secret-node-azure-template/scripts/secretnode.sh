429022ba54f7fa6bb09193eb120a40f509a069ef
Reuven Podmazo <reuven@enigma.co>
2020-07-08 17:10:54 +0300

    Update secretnode.sh

--- packaging_docker/testnet/azure/secret-node-azure-template/scripts/secretnode.sh
+++ packaging_docker/testnet/azure/secret-node-azure-template/scripts/secretnode.sh
@@ -59,7 +59,7 @@ echo "export REGISTRATION_SERVICE=$6" >> /home/"$1"/.bashrc
 # echo "export GENESIS_PATH=$5" >> /home/$1/.bashrc
 
 echo 'alias secretcli="docker exec -it secret-node_node_1 secretcli"' >> /home/"$1"/.bashrc
-echo 'alias secretcli="docker exec -it secret-node_node_1 secretd"' >> /home/"$1"/.bashrc
+echo 'alias secretd="docker exec -it secret-node_node_1 secretd"' >> /home/"$1"/.bashrc
 
 export CHAINID=$2
 export MONIKER=$3
