1ce17e522b5faf5f75f02314522493486e6a0a0c
Cashmaney <itzgro@gmail.com>
2020-07-14 12:25:13 +0300

    Update secretnode.sh

--- packaging_docker/testnet/azure/secret-node-azure-template/scripts/secretnode.sh
+++ packaging_docker/testnet/azure/secret-node-azure-template/scripts/secretnode.sh
@@ -63,7 +63,7 @@ echo "Setting Secret Node environment variables" >> /home/$1/install.progress.tx
 echo 'alias secretcli="docker exec -it secret-node_node_1 secretcli"' >> /home/"$1"/.bashrc
 echo 'alias secretd="docker exec -it secret-node_node_1 secretd"' >> /home/"$1"/.bashrc
 echo 'alias show-node-id="docker exec -it bootstrap secretd tendermint show-node-id"' >> /home/"$1"/.bashrc
-echo 'alias show-validator="docker exec -it bootstrap secretd tendermint show-node-id"' >> /home/"$1"/.bashrc
+echo 'alias show-validator="docker exec -it bootstrap secretd tendermint show-validator"' >> /home/"$1"/.bashrc
 
 echo 'alias stop-secret-node="docker-compose -f /usr/local/bin/secret-node/docker-compose.yaml down"' >> /home/"$1"/.bashrc
 echo 'alias start-secret-node="docker-compose -f /usr/local/bin/secret-node/docker-compose.yaml up -d"' >> /home/"$1"/.bashrc
