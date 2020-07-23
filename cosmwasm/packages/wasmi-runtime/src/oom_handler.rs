8e94027d3580f80d99b6097191aeae073ec5780d
Assaf Morami <assaf.morami@gmail.com>
2020-07-23 14:13:24 +0300

    Use AtomicBool instead of thread_local
    thread_local cannot be used with TCSPolicy=1, see the comments on this issue https://github.com/apache/incubator-teaclave-sgx-sdk/issues/240
    
    Co-authored-by: Reuven Podmazo <reuven@enigma.co>

--- cosmwasm/packages/wasmi-runtime/src/oom_handler.rs
+++ cosmwasm/packages/wasmi-runtime/src/oom_handler.rs
@@ -1,23 +1,23 @@
+use core::sync::atomic::{AtomicBool, Ordering};
+use std::backtrace::{self, PrintFormat};
+
 // This is thread_local to prevent race conditions in case multiple
 // threads are running while OOM happens
 // Mutex won't work because you cannot hold the Mutex between throwing
 // and catching the panic
-thread_local! {
-    static OOM_HAPPANED: std::cell::RefCell<bool> = std::cell::RefCell::new(false);
-}
+static OOM_HAPPANED: AtomicBool = AtomicBool::new(false);
 
 pub fn register_oom_handler() {
     return_and_clear_oom_happened();
     std::alloc::set_alloc_error_hook(|layout| {
-        OOM_HAPPANED.with(|oom| oom.replace(true));
+        OOM_HAPPANED.store(true, Ordering::SeqCst);
+
+        let _ = backtrace::enable_backtrace("librust_cosmwasm_enclave.so", PrintFormat::Full);
+
         panic!("memory allocation of {} bytes failed\n", layout.size());
     });
 }
 
-// pub fn is_oom_happened() -> bool {
-//     OOM_HAPPANED.with(|oom| *oom.borrow())
-// }
-
 pub fn return_and_clear_oom_happened() -> bool {
-    OOM_HAPPANED.with(|oom| oom.replace(false))
+    OOM_HAPPANED.swap(false, Ordering::SeqCst)
 }
