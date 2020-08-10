2d020f31b5d89ad7a58ba64e5f3b2c8b0770a21e
Reuven Podmazo <reuven@enigma.co>
2020-08-10 18:29:08 +0300

    wip

--- cosmwasm/packages/wasmi-runtime/src/cosmwasm/encoding.rs
+++ cosmwasm/packages/wasmi-runtime/src/cosmwasm/encoding.rs
@@ -22,7 +22,10 @@ impl Binary {
     /// fails if it is not valid base64
     pub fn from_base64(encoded: &str) -> Result<Self, EnclaveError> {
         let binary = base64::decode(encoded).map_err(|err| {
-            error!("Failed to decode base64 string: {:?}", err.to_string());
+            error!(
+                "Failed to decode base64 foo bar string: {:?}",
+                err.to_string()
+            );
             EnclaveError::FailedToDeserialize
         })?;
         Ok(Binary(binary))
@@ -86,8 +89,8 @@ impl<'de> de::Visitor<'de> for Base64Visitor {
     }
 
     fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
-        where
-            E: de::Error,
+    where
+        E: de::Error,
     {
         match Binary::from_base64(v) {
             Ok(binary) => Ok(binary),
