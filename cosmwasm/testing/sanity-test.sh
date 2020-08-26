6992222f140715c973533952d41e32a2a48c48ef
Assaf Morami <assaf.morami@gmail.com>
2020-08-26 13:28:27 +0300

    fix docker bash sanity test?

--- cosmwasm/testing/sanity-test.sh
+++ cosmwasm/testing/sanity-test.sh
@@ -102,7 +102,7 @@ yes |
 ./secretcli q compute query "$CONTRACT_ADDRESS" "{\"balance\":{\"address\":\"secret1f395p0gg67mmfd5zcqvpnp9cxnu0hg6rjep44t\"}}" |
     jq -e '.balance == "63"'
 
-(./secretcli q compute query "$CONTRACT_ADDRESS" "{\"balance\":{\"address\":\"secret1zzzzzzzzzzzzzzzzzz\"}}" || true) 2>&1 | grep 'canonicalize_address errored: invalid checksum'
+(./secretcli q compute query "$CONTRACT_ADDRESS" "{\"balance\":{\"address\":\"secret1zzzzzzzzzzzzzzzzzz\"}}" || true) 2>&1 | grep -c 'canonicalize_address errored: invalid checksum'
 
 # sleep infinity
 
