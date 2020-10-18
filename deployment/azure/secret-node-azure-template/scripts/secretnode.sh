a90d872ee668c9ca50f335e4eb3435e117c4bf63
Itzik Grossman <itzik@keytango.io>
2020-10-18 12:46:00 +0300

    Fix azure marketplace stuff

--- deployment/azure/secret-node-azure-template/scripts/secretnode.sh
+++ deployment/azure/secret-node-azure-template/scripts/secretnode.sh
@@ -44,14 +44,14 @@ echo "Creating secret node runner" >> /home/"$1"/install.progress.txt
 
 mkdir -p /usr/local/bin/secret-node
 
+sudo cp "$7" /usr/local/bin/secret-node/docker-compose.yaml
 mainnetstr="mainnet"
 if test "${6#*$mainnetstr}" != "$6"
 then
-  sudo curl -L https://raw.githubusercontent.com/enigmampc/SecretNetwork/master/deployment/azure/mainnet/docker-compose.yaml -o /usr/local/bin/secret-node/docker-compose.yaml
-  echo "Downloaded mainnet compose file" >> /home/"$1"/install.progress.txt
+  echo "Running with mainnet config" >> /home/"$1"/install.progress.txt
 else
-  sudo curl -L https://raw.githubusercontent.com/enigmampc/SecretNetwork/master/deployment/azure/testnet/docker-compose.yaml -o /usr/local/bin/secret-node/docker-compose.yaml
-  echo "Downloaded testnet compose file" >> /home/"$1"/install.progress.txt
+  # leaving this here as a placeholder for future versions where we might have to change stuff for testnet vs. mainnet
+  echo "Running with testnet config" >> /home/"$1"/install.progress.txt
 fi
 
 
