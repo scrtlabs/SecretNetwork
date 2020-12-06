a036a79fd39bf4695f121d468f8e13d118b475f3
Assaf Morami <assaf.morami@gmail.com>
2020-12-06 13:31:02 +0200

    devenv revet keys to be randomly generated

--- deployment/docker/bootstrap/bootstrap_init.sh
+++ deployment/docker/bootstrap/bootstrap_init.sh
@@ -17,14 +17,10 @@ if [ ! -e "$file" ]; then
   cp ~/node_key.json ~/.secretd/config/node_key.json
 
   perl -i -pe 's/"stake"/"uscrt"/g' ~/.secretd/config/genesis.json
-  echo flame satoshi grace olive stove economy prepare hurdle end mandate sure daring more armor family produce rural quick winter monster armor hollow impose skin |
-    secretcli keys add a --recover
-  echo wrist mansion remain topic trick monitor olympic auction piano entry sheriff dial trash armed arrow few welcome pole clown mesh party squeeze bridge exhibit |
-    secretcli keys add b --recover
-  echo shadow lamp orphan upgrade improve tomorrow secret eight dinosaur hotel believe tunnel emotion problem angry goose grace expire suggest slide federal million major prevent |
-    secretcli keys add c --recover
-  echo quote coffee avocado reflect latin gather powder outside pudding idle era surge stock second mandate pilot another promote sword ordinary coconut hospital drift spawn |
-    secretcli keys add d --recover
+  secretcli keys add a
+  secretcli keys add b
+  secretcli keys add c
+  secretcli keys add d
 
   secretd add-genesis-account "$(secretcli keys show -a a)" 1000000000000000000uscrt
   secretd add-genesis-account "$(secretcli keys show -a b)" 1000000000000000000uscrt
