203719d0ee065fbe2bcaa218885947a18f388edc
Cashmaney <itzik@enigma.co>
2021-08-01 15:06:24 +0300

    Fixed reflect test

--- x/compute/internal/keeper/reflect_test.go
+++ x/compute/internal/keeper/reflect_test.go
@@ -291,15 +291,15 @@ func TestMaskReflectCustomQuery(t *testing.T) {
 	ownerQuery := MaskQueryMsg{
 		Owner: &struct{}{},
 	}
-	_, err = json.Marshal(ownerQuery)
+	ownerQueryBz, err := json.Marshal(ownerQuery)
 	require.NoError(t, err)
 
-	//ownerRes, qErr := queryHelper(t, keeper, ctx, contractAddr, string(ownerQueryBz), true, defaultGasForTests)
-	//require.Empty(t, qErr)
-	//var res OwnerResponse
-	//err = json.Unmarshal([]byte(ownerRes), &res)
-	//require.NoError(t, err)
-	//assert.Equal(t, res.Owner, creator.String())
+	ownerRes, qErr := queryHelper(t, keeper, ctx, contractAddr, string(ownerQueryBz), true, defaultGasForTests)
+	require.Empty(t, qErr)
+	var res OwnerResponse
+	err = json.Unmarshal([]byte(ownerRes), &res)
+	require.NoError(t, err)
+	assert.Equal(t, res.Owner, creator.String())
 
 	// and now making use of the custom querier callbacks
 	customQuery := MaskQueryMsg{
@@ -444,87 +444,3 @@ func performCustomQuery(_ sdk.Context, request json.RawMessage) ([]byte, error) 
 
 	return nil, sdkerrors.Wrap(types.ErrInvalidMsg, "Unknown Custom query variant")
 }
-
-//type maskCustomMsg struct {
-//	Debug string `json:"debug,omitempty"`
-//	Raw   []byte `json:"raw,omitempty"`
-//}
-//
-//// toMaskRawMsg encodes an sdk msg using amino json encoding.
-//// Then wraps it as an opaque message
-//func toMaskRawMsg(cdc codec.Marshaler, msg sdk.Msg) (wasmTypes.CosmosMsg, error) {
-//	rawBz, err := cdc.MarshalJSON(msg)
-//	if err != nil {
-//		return wasmTypes.CosmosMsg{}, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
-//	}
-//	customMsg, err := json.Marshal(maskCustomMsg{
-//		Raw: rawBz,
-//	})
-//	res := wasmTypes.CosmosMsg{
-//		Custom: customMsg,
-//	}
-//	return res, nil
-//}
-//
-//// maskEncoders needs to be registered in test setup to handle custom message callbacks
-//func maskEncoders(cdc codec.Marshaler) *MessageEncoders {
-//	return &MessageEncoders{
-//		Custom: fromMaskRawMsg(cdc),
-//	}
-//}
-//
-//// fromMaskRawMsg decodes msg.Data to an sdk.Msg using amino json encoding.
-//// this needs to be registered on the Encoders
-//func fromMaskRawMsg(cdc codec.Marshaler) CustomEncoder {
-//	return func(_sender sdk.AccAddress, msg json.RawMessage) ([]sdk.Msg, error) {
-//		var custom maskCustomMsg
-//		err := json.Unmarshal(msg, &custom)
-//		if err != nil {
-//			return nil, sdkerrors.Wrap(sdkerrors.ErrJSONUnmarshal, err.Error())
-//		}
-//		if custom.Raw != nil {
-//			var sdkMsg sdk.Msg
-//			err := cdc.UnmarshalJSON(custom.Raw, sdkMsg)
-//			if err != nil {
-//				return nil, sdkerrors.Wrap(sdkerrors.ErrJSONUnmarshal, err.Error())
-//			}
-//			return []sdk.Msg{sdkMsg}, nil
-//		}
-//		if custom.Debug != "" {
-//			return nil, sdkerrors.Wrapf(types.ErrInvalidMsg, "Custom Debug: %s", custom.Debug)
-//		}
-//		return nil, sdkerrors.Wrap(types.ErrInvalidMsg, "Unknown Custom message variant")
-//	}
-//}
-//
-//type maskCustomQuery struct {
-//	Ping    *struct{} `json:"ping,omitempty"`
-//	Capital *Text     `json:"capital,omitempty"`
-//}
-//
-//type customQueryResponse struct {
-//	Msg string `json:"msg"`
-//}
-//
-//// maskPlugins needs to be registered in test setup to handle custom query callbacks
-//func maskPlugins() *QueryPlugins {
-//	return &QueryPlugins{
-//		Custom: performCustomQuery,
-//	}
-//}
-//
-//func performCustomQuery(_ sdk.Context, request json.RawMessage) ([]byte, error) {
-//	var custom maskCustomQuery
-//	err := json.Unmarshal(request, &custom)
-//	if err != nil {
-//		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONUnmarshal, err.Error())
-//	}
-//	if custom.Capital != nil {
-//		msg := strings.ToUpper(custom.Capital.Text)
-//		return json.Marshal(customQueryResponse{Msg: msg})
-//	}
-//	if custom.Ping != nil {
-//		return json.Marshal(customQueryResponse{Msg: "pong"})
-//	}
-//	return nil, sdkerrors.Wrap(types.ErrInvalidMsg, "Unknown Custom query variant")
-//}
