7748092b7c3541cb8d0b9c6259a7a9e0478c5894
Reuven Podmazo <reuven@enigma.co>
2022-02-05 19:23:07 +0200

    fixed enclave test compilation 2

--- cosmwasm/packages/sgx-vm/src/enclave_tests.rs
+++ cosmwasm/packages/sgx-vm/src/enclave_tests.rs
@@ -1,14 +1,20 @@
 use sgx_types;
 use sgx_types::{sgx_enclave_id_t, sgx_status_t, SgxResult};
 
-use crate::enclave::get_enclave;
+use crate::enclave::QUERY_DOORBELL;
 
 extern "C" {
     pub fn ecall_run_tests(eid: sgx_enclave_id_t, retval: *mut u32) -> sgx_status_t;
 }
 
 pub fn run_tests() -> SgxResult<u32> {
-    let enclave = get_enclave()?;
+    // Bind the token to a local variable to ensure its
+    // destructor runs in the end of the function
+    let enclave_access_token = QUERY_DOORBELL
+        .get_access(false) // This can never be recursive
+        .ok_or(sgx_status_t::SGX_ERROR_BUSY)?;
+    let enclave = (*enclave_access_token)?;
+
     let mut failed_tests = 0;
     let status = unsafe { ecall_run_tests(enclave.geteid(), &mut failed_tests) };
     match status {
