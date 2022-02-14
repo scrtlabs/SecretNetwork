e401ec84fe9d5266266295697889eff3aa5f303c
Reuven Podmazo <reuven@enigma.co>
2022-02-14 16:10:20 +0200

    Revert "Attempt to write query logs to a file"
    This reverts commit d66a2d0ead2a41417d0938019a0bc4a6026d6c29.

--- cosmwasm/enclaves/shared/utils/src/logger.rs
+++ cosmwasm/enclaves/shared/utils/src/logger.rs
@@ -1,7 +1,4 @@
 use log::{Metadata, Record};
-use std::io::Write;
-use std::sync::SgxMutex;
-use std::untrusted::fs::File;
 
 pub const LOG_LEVEL_ENV_VAR: &str = "LOG_LEVEL";
 
@@ -20,37 +17,6 @@ impl log::Log for SimpleLogger {
             record.target(),
             record.args()
         );
-    }
-
-    fn flush(&self) {}
-}
-
-pub struct FileLogger {
-    file: &'static SgxMutex<File>,
-}
-
-impl FileLogger {
-    pub fn new(file: &'static SgxMutex<File>) -> Self {
-        FileLogger { file }
-    }
-}
-
-impl log::Log for FileLogger {
-    fn enabled(&self, _metadata: &Metadata) -> bool {
-        // Not really needed since we set logging level at lib.rs in the init function
-        true
-    }
-
-    fn log(&self, record: &Record) {
-        let mut file = self.file.lock().unwrap();
-        writeln!(
-            file,
-            "{}  [{}] {}",
-            record.level(),
-            record.target(),
-            record.args()
-        )
-        .expect("Failed to write to query enclave log file");
     }
 
     fn flush(&self) {}
