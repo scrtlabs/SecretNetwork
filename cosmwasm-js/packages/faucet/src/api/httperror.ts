ce736eaa2dd5e160c240518b565ebdc2d8a0fb19
Assaf Morami <assaf.morami@gmail.com>
2022-02-23 08:23:25 +0200

    rm cosmwasm-js

--- cosmwasm-js/packages/faucet/src/api/httperror.ts
+++
@@ -1,5 +0,0 @@
-export class HttpError extends Error {
-  constructor(public readonly status: number, text: string, public readonly expose: boolean = true) {
-    super(text);
-  }
-}
--- cosmwasm-js/packages/faucet/src/types.ts
+++
@@ -1,7 +0,0 @@
-import { Address, Amount, Identity } from "@iov/bcp";
-
-export interface SendJob {
-  readonly sender: Identity;
-  readonly recipient: Address;
-  readonly amount: Amount;
-}
