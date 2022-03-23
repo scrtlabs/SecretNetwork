b737280f111c180aef3bbd6250b18a4324d88078
Assaf Morami <assaf.morami@gmail.com>
2022-03-23 14:01:19 +0200

    Remove old secret.js

--- cosmwasm-js/packages/sdk/types/ProtoEncoding.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class MsgData {
-  msgType: string;
-  data: string | undefined;
-  constructor(msgType: string, data: string | undefined);
-}
-export declare function decodeTxData(data: Uint8Array): MsgData[];
