0d8079727434036d043cfe2d592922f6b56883ef
Assaf Morami <assaf.morami@gmail.com>
2022-04-27 22:28:15 +0300

    Make WasmSnapshotter use the existing API
    Rather than going staright to the filesystem

--- app/app.go
+++ app/app.go
@@ -622,9 +622,14 @@ func NewSecretNetworkApp(
 	app.ScopedIBCKeeper = scopedIBCKeeper
 	app.ScopedTransferKeeper = scopedTransferKeeper
 
-	app.SnapshotManager().RegisterExtensions(
-		compute.NewWasmSnapshotter(filepath.Join(computeDir, "wasm", "wasm")),
-	)
+	if manager := app.SnapshotManager(); manager != nil {
+		err := manager.RegisterExtensions(
+			compute.NewWasmSnapshotter(app.CommitMultiStore(), &app.computeKeeper),
+		)
+		if err != nil {
+			panic(fmt.Errorf("failed to register snapshot extension: %s", err))
+		}
+	}
 
 	return app
 }
