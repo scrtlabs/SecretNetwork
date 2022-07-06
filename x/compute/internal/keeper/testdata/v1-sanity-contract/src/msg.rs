1a8262c735fe0e8bc94a5a28e85d5045360ff003
Assaf Morami <assaf.morami@gmail.com>
2022-07-06 12:56:01 +0300

    InstantiateMsg snake_case in v1-sanity-contract

--- x/compute/internal/keeper/testdata/v1-sanity-contract/src/msg.rs
+++ x/compute/internal/keeper/testdata/v1-sanity-contract/src/msg.rs
@@ -3,6 +3,7 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
 pub enum InstantiateMsg {
     Counter {
         counter: u64,
