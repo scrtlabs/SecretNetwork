1d658903df9c07664074201812236c10ed204f5f
Taariq Levack <taariql@gmail.com>
2022-07-12 17:01:24 +0200

    Port dev image faucet from python to node #957

--- deployment/docker/devimage/bootstrap_init_no_stop.sh
+++ deployment/docker/devimage/bootstrap_init_no_stop.sh
@@ -65,7 +65,7 @@ perl -i -pe 's/enable-unsafe-cors = false/enable-unsafe-cors = true/' .secretd/c
 lcp --proxyUrl http://localhost:1316 --port 1317 --proxyPartial '' &
 
 # Setup faucet
-gunicorn --bind 0.0.0.0:5000 svc &
+nohup node faucet_server.js >/dev/null 2>&1 &
 
 # Setup secretcli
 cp $(which secretd) $(dirname $(which secretd))/secretcli
