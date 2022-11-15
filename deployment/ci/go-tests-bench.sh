2eee440af35354e74505c5a4dd6eddce6ae04a8d
Itzik <itzik@scrtlabs.com>
2022-11-15 21:41:43 +0200

    Fix go tests in ci

--- deployment/ci/go-tests-bench.sh
+++ deployment/ci/go-tests-bench.sh
@@ -2,4 +2,4 @@ set -euv
 
 # Run bench mark tests
 # go test -count 1 -v ./x/compute/internal/... -run "TestRunBenchmarks"
-LOG_LEVEL=ERROR go test -count 1 -v ./x/compute/internal/... -run "TestRunBenchmarks"
+LOG_LEVEL=ERROR go test -tags "test" -count 1 -v ./x/compute/internal/... -run "TestRunBenchmarks"
