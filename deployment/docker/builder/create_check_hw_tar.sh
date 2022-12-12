52f8d41bdce0b0cdad3474c3796ab9e60da691a5
TomL94 <langer.tom7@gmail.com>
2022-12-12 18:29:07 +0200

    check-hw: fix script to include readme

--- deployment/docker/builder/create_check_hw_tar.sh
+++ deployment/docker/builder/create_check_hw_tar.sh
@@ -3,7 +3,7 @@
 mkdir -p /build/check-hw/
 cp ./check-hw /build/check-hw/
 cp ./check_hw_enclave.so /build/check-hw/
-cp ./README /build/check-hw/
+cp ./README.md /build/check-hw/
 
 cd /build/
 tar -czf check_hw_"$VERSION".tar.gz check-hw
