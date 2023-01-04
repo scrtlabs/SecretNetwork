bff34239eab1a785de8287cdd98b5a5d8d39a73a
Cashmaney <itzik@enigma.co>
2023-01-04 14:13:05 +0200

    Fixed multisend test (fu assaf's tests)

--- integration-tests/test.ts
+++ integration-tests/test.ts
@@ -518,6 +518,8 @@ describe("CustomMsg", () => {
 
 describe("tx broadcast multi", () => {
   test("Send Multiple Messages Amino", async () => {
+    const { validators } = await readonly.query.staking.validators({});
+    const validator = validators[0].operatorAddress;
 
     let tx = await accounts[0].secretjs.tx.broadcast(
         [
@@ -526,12 +528,18 @@ describe("tx broadcast multi", () => {
             toAddress: accounts[0].address,
             amount: [{ denom: "uscrt", amount: "1" }],
           }),
+
           new MsgExecuteContract({
-            sender: accounts[0].secretjs.address,
-            contractAddress: contracts["secretdev-1"].v010.address,
+            sender: accounts[0].address,
+            contractAddress: contracts["secretdev-1"].v1.address,
+            codeHash: contracts["secretdev-1"].v1.codeHash,
             msg: {
-              custom_msg: {},
+              staking_msg_delegate: {
+                validator: validator,
+                amount: { amount: "1", denom: "uscrt" },
+              },
             },
+            sentFunds: [{ amount: "1", denom: "uscrt" }],
           }),
         ],
         {
@@ -542,6 +550,7 @@ describe("tx broadcast multi", () => {
     if (tx.code !== TxResultCode.Success) {
       console.error(tx.rawLog);
     }
+
     expect(tx.code).toBe(TxResultCode.Success);
   });
 });
