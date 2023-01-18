703634821695639e09fb3074d687cd9937814664
Lior Bondarevski <liorbond@gmail.com>
2023-01-18 15:18:08 +0200

    Fix

--- cosmwasm/enclaves/execute/src/registration/offchain.rs
+++ cosmwasm/enclaves/execute/src/registration/offchain.rs
@@ -4,8 +4,6 @@
 ///
 use log::*;
 use sgx_types::sgx_status_t;
-#[cfg(all(feature = "SGX_MODE_HW", not(feature = "production")))]
-use sgx_types::{sgx_platform_info_t, sgx_update_info_bit_t};
 use std::slice;
 
 use enclave_crypto::consts::{
