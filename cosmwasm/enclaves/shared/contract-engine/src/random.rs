07c2bb27e872d658aecf7658f15765c8510fdd5a
Eshel <eshel@scrtlabs.com>
2023-01-30 23:07:43 +0200

    using message counter to derive random, and incrementing it on every usage

--- cosmwasm/enclaves/shared/contract-engine/src/random.rs
+++ cosmwasm/enclaves/shared/contract-engine/src/random.rs
@@ -1,6 +1,7 @@
 use crate::contract_validation::ContractKey;
 use cw_types_v010::encoding::Binary;
 use lazy_static::lazy_static;
+use log::trace;
 
 use std::sync::SgxMutex;
 
@@ -22,12 +23,28 @@ pub fn derive_random(seed: &Binary, contract_key: &ContractKey, height: u64) -> 
         counter.counter = 0;
     }
 
+    trace!("counter used to derive random for height {}: {:?}", height, counter);
+
     let height_bytes = height.to_be_bytes();
-    let data = vec![height_bytes.as_slice(), &contract_key.as_slice()];
+    let counter_bytes = counter.counter.to_be_bytes();
+    let data = vec![
+        height_bytes.as_slice(),
+        &contract_key.as_slice(),
+        counter_bytes.as_slice()
+    ];
 
     Binary(
         enclave_crypto::hkdf_sha_256(&seed.0.as_slice(), data.as_slice())
             .get()
             .to_vec(),
     )
+}
+
+pub fn update_msg_counter(height: u64) {
+    let mut counter = MSG_COUNTER.lock().unwrap();
+
+    counter.height = height;
+    counter.counter += 1;
+
+    trace!("counter incremented to: {:?}", counter);
 }
