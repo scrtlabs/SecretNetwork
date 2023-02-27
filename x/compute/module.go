c41035a2906ffb356b8e33064bb2a9496b8ce626
Lior Bondarevski <liorbond@gmail.com>
2023-02-28 00:16:16 +0200

    Print using logger instead of printfs

--- x/compute/module.go
+++ x/compute/module.go
@@ -3,7 +3,6 @@ package compute
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"math/rand"
 
 	"github.com/scrtlabs/SecretNetwork/go-cosmwasm/api"
@@ -164,7 +163,7 @@ func (am AppModule) BeginBlock(ctx sdk.Context, beginBlock abci.RequestBeginBloc
 	// In this case Marshal will fail with a Seg Fault.
 	// The fix below it a temporary fix until we will investigate the issue in tendermint.
 	if beginBlock.Commit == nil {
-		fmt.Printf("skipping commit submition to the enclave for block %d\n", beginBlock.Header.Height)
+		ctx.Logger().Info("skipping commit submition to the enlave for block %d\n", beginBlock.Header.Height)
 		return
 	}
 
