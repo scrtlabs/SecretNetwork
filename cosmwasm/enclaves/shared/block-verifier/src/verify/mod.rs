b6572e69ade9ba73a88af89b5533bd65a997dc32
Cashmaney <itzik@scrtlabs.com>
2023-06-01 10:02:57 +0300

    Verify new message type2 (#1452)
    * Add validation for new message type

--- cosmwasm/enclaves/shared/block-verifier/src/verify/mod.rs
+++ cosmwasm/enclaves/shared/block-verifier/src/verify/mod.rs
@@ -1,8 +1,11 @@
-pub mod txs;
-pub mod validator_set;
 pub mod block;
 pub mod commit;
 pub mod header;
+pub mod txs;
+pub mod validator_set;
 
 #[cfg(feature = "random")]
 pub mod random;
+
+// external messages
+pub mod registration;
