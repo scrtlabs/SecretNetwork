fbc872f4036c33a66f3782562db806836d8988b0
Assaf Morami <assaf.morami@gmail.com>
2023-07-18 10:04:45 +0300

    Fix v0.10 test migrate contract

--- cosmwasm/contracts/v010/compute-tests/test-compute-contract-v2/src/lib.rs
+++ cosmwasm/contracts/v010/compute-tests/test-compute-contract-v2/src/lib.rs
@@ -4,7 +4,7 @@ pub mod contract;
 mod wasm {
     use super::contract;
     use cosmwasm_std::{
-        do_handle, do_init, do_query, ExternalApi, ExternalQuerier, ExternalStorage,
+        do_handle, do_init, do_migrate, do_query, ExternalApi, ExternalQuerier, ExternalStorage,
     };
 
     #[no_mangle]
