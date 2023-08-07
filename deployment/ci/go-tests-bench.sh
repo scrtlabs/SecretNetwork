4f8a98da598db673208c424af5523489b0651f66
Itzik Grossman <itzik@scrtlabs.com>
2023-08-07 16:48:15 +0300

    Another attempted fix

--- deployment/ci/go-tests-bench.sh
+++ deployment/ci/go-tests-bench.sh
@@ -1,6 +1,7 @@
 set -euv
 
+export LD_LIBRARY_PATH=/usr/lib:/usr/local/lib:/opt/sgxsdk/sdk_libs:/usr/lib/x86_64-linux-gnu/
 # Run bench mark tests
 # go test -count 1 -v ./x/compute/internal/... -run "TestRunBenchmarks"
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu/ LOG_LEVEL=ERROR go test -tags "sgx hw test" -count 1 -v ./x/compute/internal/... -run "TestRunExecuteBenchmarks"
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu/ LOG_LEVEL=ERROR go test -tags "sgx hw test" -count 1 -v ./x/compute/internal/... -run "TestRunQueryBenchmarks"
+LOG_LEVEL=ERROR go test -tags "sgx hw test" -count 1 -v ./x/compute/internal/... -run "TestRunExecuteBenchmarks"
+LOG_LEVEL=ERROR go test -tags "sgx hw test" -count 1 -v ./x/compute/internal/... -run "TestRunQueryBenchmarks"
--- deployment/ci/go-tests.sh
+++ deployment/ci/go-tests.sh
@@ -1,5 +1,6 @@
 set -euv
 
+export LD_LIBRARY_PATH=/usr/lib:/usr/local/lib:/opt/sgxsdk/sdk_libs:/usr/lib/x86_64-linux-gnu/
 # Run go system tests for compute module
 mkdir -p ./x/compute/internal/keeper/.sgx_secrets
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu/ LOG_LEVEL=ERROR GOMAXPROCS=8 SCRT_SGX_STORAGE='./' go test -tags "sgx hw test" -failfast -timeout 90m -v ./x/compute/internal/...
+LOG_LEVEL=ERROR GOMAXPROCS=8 SCRT_SGX_STORAGE='./' go test -tags "sgx hw test" -failfast -timeout 90m -v ./x/compute/internal/...
