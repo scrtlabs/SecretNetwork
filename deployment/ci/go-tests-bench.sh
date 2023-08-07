1426ea349e9485f74fa830e7260bad7970f15378
Itzik Grossman <itzik@scrtlabs.com>
2023-08-07 15:07:36 +0300

    Another attempted fix

--- deployment/ci/go-tests-bench.sh
+++ deployment/ci/go-tests-bench.sh
@@ -2,5 +2,5 @@ set -euv
 
 # Run bench mark tests
 # go test -count 1 -v ./x/compute/internal/... -run "TestRunBenchmarks"
-LOG_LEVEL=ERROR go test -tags "sgx hw test" -count 1 -v ./x/compute/internal/... -run "TestRunExecuteBenchmarks"
-LOG_LEVEL=ERROR go test -tags "sgx hw test" -count 1 -v ./x/compute/internal/... -run "TestRunQueryBenchmarks"
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu/ LOG_LEVEL=ERROR go test -tags "sgx hw test" -count 1 -v ./x/compute/internal/... -run "TestRunExecuteBenchmarks"
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu/ LOG_LEVEL=ERROR go test -tags "sgx hw test" -count 1 -v ./x/compute/internal/... -run "TestRunQueryBenchmarks"
