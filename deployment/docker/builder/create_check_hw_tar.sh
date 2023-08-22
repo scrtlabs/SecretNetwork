38a6fe05b8ba2d32d4b9ea97b2efbae6ebf697a5
Itzik Grossman <itzik@scrtlabs.com>
2023-08-22 23:03:12 +0300

    Check HW CI & build should work now

--- deployment/docker/builder/create_check_hw_tar.sh
+++ deployment/docker/builder/create_check_hw_tar.sh
@@ -4,7 +4,7 @@ mkdir -p /build/check-hw/
 cp ./check-hw/check-hw /build/check-hw/
 cp ./check-hw/check_hw_enclave.so /build/check-hw/
 cp ./check-hw/check_hw_enclave_testnet.so /build/check-hw/
-cp ./README.md /build/check-hw/
+cp ./check-hw/README.md /build/check-hw/
 
 cd /build/
 tar -czf check_hw_"$VERSION".tar.gz check-hw
