60ea3ace9419a283ee693acfde279a3a7fa04bf4
Itzik Grossman <itzik@scrtlabs.com>
2023-08-29 00:26:54 +0300

    gofumpt

--- app/upgrades/v1.10/upgrade.go
+++ app/upgrades/v1.10/upgrade.go
@@ -19,7 +19,7 @@ var Upgrade = upgrades.Upgrade{
 	StoreUpgrades:        store.StoreUpgrades{},
 }
 
-func createUpgradeHandler(mm *module.Manager, keepers *keepers.SecretAppKeepers, configurator module.Configurator,
+func createUpgradeHandler(mm *module.Manager, _ *keepers.SecretAppKeepers, configurator module.Configurator,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		ctx.Logger().Info(` _    _ _____   _____ _____            _____  ______ `)
