5a811042c30df336db43f4e78af5f96623f6c8ac
Itzik Grossman <itzik@scrtlabs.com>
2023-08-29 00:25:21 +0300

    gofumpt

--- app/upgrades/v1.10/upgrade.go
+++ app/upgrades/v1.10/upgrade.go
@@ -9,7 +9,6 @@ import (
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 	"github.com/scrtlabs/SecretNetwork/app/keepers"
 	"github.com/scrtlabs/SecretNetwork/app/upgrades"
-	ibchookstypes "github.com/scrtlabs/SecretNetwork/x/ibc-hooks/types"
 )
 
 const upgradeName = "v1.10"
@@ -17,14 +16,10 @@ const upgradeName = "v1.10"
 var Upgrade = upgrades.Upgrade{
 	UpgradeName:          upgradeName,
 	CreateUpgradeHandler: createUpgradeHandler,
-	StoreUpgrades: store.StoreUpgrades{
-		Added: []string{
-			ibchookstypes.StoreKey,
-		},
-	},
+	StoreUpgrades:        store.StoreUpgrades{},
 }
 
-func createUpgradeHandler(mm *module.Manager, _ *keepers.SecretAppKeepers, configurator module.Configurator,
+func createUpgradeHandler(mm *module.Manager, keepers *keepers.SecretAppKeepers, configurator module.Configurator,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		ctx.Logger().Info(` _    _ _____   _____ _____            _____  ______ `)
@@ -33,9 +28,6 @@ func createUpgradeHandler(mm *module.Manager, _ *keepers.SecretAppKeepers, confi
 		ctx.Logger().Info(`| |  | |  ___/| | |_ |  _  /   / /\ \ | |  | |  __|  `)
 		ctx.Logger().Info(`| |__| | |    | |__| | | \ \  / ____ \| |__| | |____ `)
 		ctx.Logger().Info(` \____/|_|     \_____|_|  \_\/_/    \_\_____/|______|`)
-
-		// WASM Hooks doesn't require any initialization code:
-		// https://github.com/osmosis-labs/osmosis/blob/8b4c62a26/app/upgrades/v14/upgrades.go#L12-L21
 
 		ctx.Logger().Info(fmt.Sprintf("Running module migrations for %s...", upgradeName))
 		return mm.RunMigrations(ctx, configurator, vm)
