1db3d6f9c38cf95bf6c1c3bf3e1a7525daf801e5
Lior Bondarevski <liorbond@gmail.com>
2023-09-25 01:57:25 +0300

    "Upgrade handler to fix the prev migration problem"

--- app/app.go
+++ app/app.go
@@ -34,6 +34,7 @@ import (
 	"github.com/scrtlabs/SecretNetwork/app/upgrades"
 	v1_10 "github.com/scrtlabs/SecretNetwork/app/upgrades/v1.10"
 	v1_11 "github.com/scrtlabs/SecretNetwork/app/upgrades/v1.11"
+	v1_11_testnet_broken_state "github.com/scrtlabs/SecretNetwork/app/upgrades/v1.11-testnet-broken-state"
 	v1_11_testnet_shade_hardcoded_admins "github.com/scrtlabs/SecretNetwork/app/upgrades/v1.11-testnet-shade-hardcoded-admins"
 	v1_11_testnet_shade_hardcoded_admins_fix "github.com/scrtlabs/SecretNetwork/app/upgrades/v1.11-testnet-shade-hardcoded-admins-fix"
 	v1_3 "github.com/scrtlabs/SecretNetwork/app/upgrades/v1.3"
@@ -109,6 +110,7 @@ var (
 		v1_11.Upgrade,
 		v1_11_testnet_shade_hardcoded_admins.Upgrade,
 		v1_11_testnet_shade_hardcoded_admins_fix.Upgrade,
+		v1_11_testnet_broken_state.Upgrade,
 	}
 )
 
---
+++ app/upgrades/v1.11-testnet-broken-state/upgrade.go
@@ -0,0 +1,35 @@
+package v1_11_testnet_broken_state
+
+import (
+	"fmt"
+
+	store "github.com/cosmos/cosmos-sdk/store/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+	"github.com/scrtlabs/SecretNetwork/app/keepers"
+	"github.com/scrtlabs/SecretNetwork/app/upgrades"
+)
+
+const upgradeName = "v1.11-testnet-broken-state"
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName:          upgradeName,
+	CreateUpgradeHandler: createUpgradeHandler,
+	StoreUpgrades:        store.StoreUpgrades{},
+}
+
+func createUpgradeHandler(mm *module.Manager, _ *keepers.SecretAppKeepers, configurator module.Configurator,
+) upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		ctx.Logger().Info(` _    _ _____   _____ _____            _____  ______ `)
+		ctx.Logger().Info(`| |  | |  __ \ / ____|  __ \     /\   |  __ \|  ____|`)
+		ctx.Logger().Info(`| |  | | |__) | |  __| |__) |   /  \  | |  | | |__   `)
+		ctx.Logger().Info(`| |  | |  ___/| | |_ |  _  /   / /\ \ | |  | |  __|  `)
+		ctx.Logger().Info(`| |__| | |    | |__| | | \ \  / ____ \| |__| | |____ `)
+		ctx.Logger().Info(` \____/|_|     \_____|_|  \_\/_/    \_\_____/|______|`)
+
+		ctx.Logger().Info(fmt.Sprintf("Running module migrations for %s...", upgradeName))
+		return mm.RunMigrations(ctx, configurator, vm)
+	}
+}
