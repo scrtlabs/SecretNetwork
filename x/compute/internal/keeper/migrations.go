24aa62a2d26f4bd2c64711d72833e98000c803f0
Assaf Morami <assaf.morami@gmail.com>
2023-09-25 21:14:50 +0300

    Fix migration code for hardcoded admins

--- x/compute/internal/keeper/migrations.go
+++ x/compute/internal/keeper/migrations.go
@@ -1,7 +1,6 @@
 package keeper
 
 import (
-	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/scrtlabs/SecretNetwork/x/compute/internal/types"
 )
@@ -32,7 +31,10 @@ func v1GetContractKey(ctx sdk.Context, k Keeper, contractAddress sdk.AccAddress)
 //		CurrentContractKeyProof []byte
 //	}
 func (m Migrator) Migrate1to2(ctx sdk.Context) error {
-	iter := prefix.NewStore(ctx.KVStore(m.keeper.storeKey), types.ContractKeyPrefix).Iterator(nil, nil)
+	store := ctx.KVStore(m.keeper.storeKey)
+	iter := sdk.KVStorePrefixIterator(store, types.ContractKeyPrefix)
+	defer iter.Close()
+
 	for ; iter.Valid(); iter.Next() {
 		var contractAddress sdk.AccAddress = iter.Key()
 
@@ -45,6 +47,9 @@ func (m Migrator) Migrate1to2(ctx sdk.Context) error {
 			// Otherwise and if valid, adminProof is a 32 bytes array (output of sha256).
 			// For future proofing and avoiding passing null pointers to the enclave, we'll set it to a 32 bytes array of 0.
 			contractInfo.AdminProof = make([]byte, 32)
+
+			newContractInfoBz := m.keeper.cdc.MustMarshal(&contractInfo)
+			store.Set(iter.Key(), newContractInfoBz)
 		}
 
 		// get v1 contract key
