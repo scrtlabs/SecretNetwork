616819352129119bb801ed16304213cdd21af7b3
Cashmaney <itzik@fhenix.io>
2023-11-01 14:09:21 +0700

    Fix all the stuff I broke :(

--- cosmwasm/enclaves/execute/src/ecalls.rs
+++ cosmwasm/enclaves/execute/src/ecalls.rs
@@ -55,52 +55,52 @@ pub unsafe extern "C" fn ecall_submit_block_signatures(
         sgx_status_t::SGX_ERROR_ECALL_NOT_ALLOWED
     }
 }
-
-#[no_mangle]
-#[allow(unused_variables)]
-pub unsafe extern "C" fn ecall_submit_store_roots(
-    in_roots: *const u8,
-    in_roots_len: u32,
-    in_compute_root: *const u8,
-    in_compute_root_len: u32,
-) -> sgx_status_t {
-    let failed_call = || sgx_status_t::SGX_ERROR_INVALID_PARAMETER;
-
-    validate_input_length!(in_roots_len, "roots", MAX_ROOTS_LENGTH, failed_call());
-    validate_const_ptr!(
-        in_roots,
-        in_roots_len as usize,
-        sgx_status_t::SGX_ERROR_INVALID_PARAMETER
-    );
-    validate_input_length!(
-        in_compute_root_len,
-        "compute_root",
-        MAX_MERKLE_ROOT_LENGTH,
-        failed_call()
-    );
-    validate_const_ptr!(
-        in_compute_root,
-        in_compute_root_len as usize,
-        sgx_status_t::SGX_ERROR_INVALID_PARAMETER
-    );
-
-    let store_roots_slice = slice::from_raw_parts(in_roots, in_roots_len as usize);
-    let compute_root_slice = slice::from_raw_parts(in_compute_root, in_compute_root_len as usize);
-
-    let store_roots: Pairs = Pairs::decode(store_roots_slice).unwrap();
-    let mut store_roots_bytes = vec![];
-
-    // Encode all key-value pairs to bytes
-    for root in store_roots.pairs {
-        store_roots_bytes.push(pair_to_bytes(root));
-    }
-
-    let h = merkle::simple_hash_from_byte_vectors(store_roots_bytes);
-    debug!("received app_hash: {:?}", h);
-    debug!("received compute_root: {:?}", compute_root_slice);
-
-    sgx_status_t::SGX_SUCCESS
-}
+//
+// #[no_mangle]
+// #[allow(unused_variables)]
+// pub unsafe extern "C" fn ecall_submit_store_roots(
+//     in_roots: *const u8,
+//     in_roots_len: u32,
+//     in_compute_root: *const u8,
+//     in_compute_root_len: u32,
+// ) -> sgx_status_t {
+//     let failed_call = || sgx_status_t::SGX_ERROR_INVALID_PARAMETER;
+//
+//     validate_input_length!(in_roots_len, "roots", MAX_ROOTS_LENGTH, failed_call());
+//     validate_const_ptr!(
+//         in_roots,
+//         in_roots_len as usize,
+//         sgx_status_t::SGX_ERROR_INVALID_PARAMETER
+//     );
+//     validate_input_length!(
+//         in_compute_root_len,
+//         "compute_root",
+//         MAX_MERKLE_ROOT_LENGTH,
+//         failed_call()
+//     );
+//     validate_const_ptr!(
+//         in_compute_root,
+//         in_compute_root_len as usize,
+//         sgx_status_t::SGX_ERROR_INVALID_PARAMETER
+//     );
+//
+//     let store_roots_slice = slice::from_raw_parts(in_roots, in_roots_len as usize);
+//     let compute_root_slice = slice::from_raw_parts(in_compute_root, in_compute_root_len as usize);
+//
+//     let store_roots: Pairs = Pairs::decode(store_roots_slice).unwrap();
+//     let mut store_roots_bytes = vec![];
+//
+//     // Encode all key-value pairs to bytes
+//     for root in store_roots.pairs {
+//         store_roots_bytes.push(pair_to_bytes(root));
+//     }
+//
+//     let h = merkle::simple_hash_from_byte_vectors(store_roots_bytes);
+//     debug!("received app_hash: {:?}", h);
+//     debug!("received compute_root: {:?}", compute_root_slice);
+//
+//     sgx_status_t::SGX_SUCCESS
+// }
 
 // This is a copy of a cosmos-sdk function: https://github.com/scrtlabs/cosmos-sdk/blob/1b9278476b3ac897d8ebb90241008476850bf212/store/internal/maps/maps.go#LL152C1-L152C1
 // Returns key || value, with both the key and value length prefixed.
