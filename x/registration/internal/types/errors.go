cbd3c0ce96dfb1b0f7317c7bc9a18b5c8af57464
Ilya Kapitonau <ilya.th.kapitonov@gmail.com>
2024-02-26 13:57:17 +0300

    Replace github.com/cosmos/cosmos-sdk/types/errors with cosmossdk.io/errors

--- x/registration/internal/types/errors.go
+++ x/registration/internal/types/errors.go
@@ -1,7 +1,7 @@
 package types
 
 import (
-	sdkErrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"cosmossdk.io/errors"
 )
 
 // Codes for wasm contract errors
@@ -9,21 +9,21 @@ var (
 	DefaultCodespace = ModuleName
 
 	// ErrSeedInitFailed error when the seed initialization fails
-	ErrSeedInitFailed = sdkErrors.Register(DefaultCodespace, 1, "Initialize node seed failed")
+	ErrSeedInitFailed = errors.Register(DefaultCodespace, 1, "Initialize node seed failed")
 
-	ErrAuthenticateFailed = sdkErrors.Register(DefaultCodespace, 2, "Failed to authenticate node")
-
-	// ErrSeedInitFailed error when the seed initialization fails
-	ErrSeedValidationParams = sdkErrors.Register(DefaultCodespace, 3, "Failed to validate seed parameters")
+	ErrAuthenticateFailed = errors.Register(DefaultCodespace, 2, "Failed to authenticate node")
 
 	// ErrSeedInitFailed error when the seed initialization fails
-	BootstrapInitFailed = sdkErrors.Register(DefaultCodespace, 4, "Failed to initialize bootstrap")
+	ErrSeedValidationParams = errors.Register(DefaultCodespace, 3, "Failed to validate seed parameters")
 
-	ErrInvalidType = sdkErrors.Register(DefaultCodespace, 5, "Type error")
+	// ErrSeedInitFailed error when the seed initialization fails
+	BootstrapInitFailed = errors.Register(DefaultCodespace, 4, "Failed to initialize bootstrap")
 
-	ErrCertificateInvalid = sdkErrors.Register(DefaultCodespace, 6, "Certificate invalid or does not exist")
+	ErrInvalidType = errors.Register(DefaultCodespace, 5, "Type error")
 
-	ErrNotFound = sdkErrors.Register(DefaultCodespace, 7, "not found")
+	ErrCertificateInvalid = errors.Register(DefaultCodespace, 6, "Certificate invalid or does not exist")
 
-	ErrInvalid = sdkErrors.Register(DefaultCodespace, 8, "invalid")
+	ErrNotFound = errors.Register(DefaultCodespace, 7, "not found")
+
+	ErrInvalid = errors.Register(DefaultCodespace, 8, "invalid")
 )
