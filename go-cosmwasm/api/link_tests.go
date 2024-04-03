c0180679ae4bb4dfe0c55ba74e1f5a64399bca75
valdok <vlad@scrtlabs.com>
2024-04-03 08:24:51 +0000

    build fix WIP(7)

--- go-cosmwasm/api/link_tests.go
+++
@@ -1,7 +0,0 @@
-//go:build !secretcli && linux && !muslc && !darwin && test
-// +build !secretcli,linux,!muslc,!darwin,test
-
-package api
-
-// #cgo LDFLAGS: -Wl,-rpath,${SRCDIR} -L${SRCDIR} -lgo_cosmwasm
-import "C"
