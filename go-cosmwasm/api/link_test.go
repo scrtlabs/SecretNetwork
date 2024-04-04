34410bf933972bdfbe45a0d9950c3b66cd5e00e5
Cashmaney <itzik@scrtlabs.com>
2024-04-04 22:04:05 +0300

    Delete go-cosmwasm/api/link_test.go

--- go-cosmwasm/api/link_test.go
+++
@@ -1,7 +0,0 @@
-//go:build !secretcli && linux && !muslc && !darwin && test
-// +build !secretcli,linux,!muslc,!darwin,test
-
-package api
-
-// #cgo LDFLAGS: -Wl,-rpath,${SRCDIR} -L${SRCDIR} -lgo_cosmwasm -lsgx_uae_service -lsgx_dcap_ql -lsgx_dcap_quoteverify
-import "C"
