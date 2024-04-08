2bf58f37fa555b1ca522eb5cbfd347cacad94ad0
valdok <vlad@scrtlabs.com>
2024-04-08 15:52:04 +0000

    check-hw build fix

--- check-hw/src/enclave_api.rs
+++ check-hw/src/enclave_api.rs
@@ -8,9 +8,9 @@ use enclave_ffi_types::{
 };
 use sgx_types::{
     c_int, sgx_calc_quote_size, sgx_enclave_id_t, sgx_epid_group_id_t, sgx_get_quote,
-    sgx_init_quote, sgx_platform_info_t, sgx_quote_nonce_t, sgx_quote_sign_type_t, sgx_quote_t,
-    sgx_report_attestation_status, sgx_report_t, sgx_spid_t, sgx_status_t, sgx_target_info_t,
-    sgx_update_info_bit_t,
+    sgx_init_quote, sgx_platform_info_t, sgx_ql_qe_report_info_t, sgx_ql_qv_result_t,
+    sgx_quote_nonce_t, sgx_quote_sign_type_t, sgx_quote_t, sgx_report_attestation_status,
+    sgx_report_t, sgx_spid_t, sgx_status_t, sgx_target_info_t, sgx_update_info_bit_t,
 };
 
 // ecalls
@@ -184,5 +184,55 @@ pub extern "C" fn ocall_read_db(
 
 #[no_mangle]
 pub extern "C" fn ocall_allocate(_buffer: *const u8, _length: usize) -> UserSpaceBuffer {
+    unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn ocall_get_quote_ecdsa_params(
+    ret_val: *mut sgx_status_t,
+    p_qe_info: *mut sgx_target_info_t,
+    p_quote_size: *mut u32,
+) -> sgx_status_t {
+    unimplemented!()
+}
+#[no_mangle]
+pub extern "C" fn ocall_get_quote_ecdsa(
+    ret_val: *mut sgx_status_t,
+    p_report: *const sgx_report_t,
+    p_quote: *mut u8,
+    n_quote: u32,
+) -> sgx_status_t {
+    unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn ocall_get_quote_ecdsa_collateral(
+    ret_val: *mut sgx_status_t,
+    p_quote: *const u8,
+    n_quote: u32,
+    p_col: *mut u8,
+    n_col: u32,
+    p_col_out: *mut u32,
+) -> sgx_status_t {
+    unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn ocall_verify_quote_ecdsa(
+    ret_val: *mut sgx_status_t,
+    p_quote: *const u8,
+    n_quote: u32,
+    p_col: *const u8,
+    n_col: u32,
+    p_target_info: *const sgx_target_info_t,
+    time_s: i64,
+    p_qve_report_info: *mut sgx_ql_qe_report_info_t,
+    p_supp_data: *mut u8,
+    n_supp_data: u32,
+    p_supp_data_size: *mut u32,
+    p_time_s: *mut i64,
+    p_collateral_expiration_status: *mut u32,
+    p_qv_result: *mut sgx_ql_qv_result_t,
+) -> sgx_status_t {
     unimplemented!()
 }
