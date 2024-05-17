afbfe654d7769df2e643a48489fa7a6bdb94a50a
Qua Is X <qua.is.kyiv.ua@gmail.com>
2024-05-17 19:19:48 -0400

    fix localsecret start script

--- deployment/docker/localsecret/bootstrap_init_no_stop.sh
+++ deployment/docker/localsecret/bootstrap_init_no_stop.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -x
+set -oe errexit
 
 ENABLE_FAUCET=${1:-"true"}
 
 custom_script_path=${POST_INIT_SCRIPT:-"/root/post_init.sh"}
 
 file=~/.secretd/config/genesis.json
-if [ ! -e "$file" ]
-then
+if [ ! -e "$file" ]; then
   # init the node
   rm -rf ~/.secretd/*
   rm -rf /opt/secret/.sgx_secrets/*
@@ -46,8 +46,8 @@ then
     .app_state.gov.deposit_params.min_deposit[0].denom = "uscrt" |
     .app_state.mint.params.mint_denom = "uscrt" |
     .app_state.staking.params.bond_denom = "uscrt"
-  ' ~/.secretd/config/genesis.json > ~/.secretd/config/genesis.json.tmp && mv ~/.secretd/config/genesis.json{.tmp,}
-  
+  ' ~/.secretd/config/genesis.json >~/.secretd/config/genesis.json.tmp && mv ~/.secretd/config/genesis.json{.tmp,}
+
   if [ "${fast_blocks}" = "true" ]; then
     sed -E -i '/timeout_(propose|prevote|precommit|commit)/s/[0-9]+m?s/200ms/' ~/.secretd/config/config.toml
   fi
@@ -60,25 +60,29 @@ then
     echo "Done running custom script!"
   fi
 
+  v_mnemonic="push certain add next grape invite tobacco bubble text romance again lava crater pill genius vital fresh guard great patch knee series era tonight"
   a_mnemonic="grant rice replace explain federal release fix clever romance raise often wild taxi quarter soccer fiber love must tape steak together observe swap guitar"
   b_mnemonic="jelly shadow frog dirt dragon use armed praise universe win jungle close inmate rain oil canvas beauty pioneer chef soccer icon dizzy thunder meadow"
   c_mnemonic="chair love bleak wonder skirt permit say assist aunt credit roast size obtain minute throw sand usual age smart exact enough room shadow charge"
   d_mnemonic="word twist toast cloth movie predict advance crumble escape whale sail such angry muffin balcony keen move employ cook valve hurt glimpse breeze brick"
 
+  echo $v_mnemonic | secretd keys add validator --recover
   echo $a_mnemonic | secretd keys add a --recover
   echo $b_mnemonic | secretd keys add b --recover
   echo $c_mnemonic | secretd keys add c --recover
   echo $d_mnemonic | secretd keys add d --recover
 
-  # secretd genesis add-genesis-account "$(secretd keys show -a a)" 1000000000000000000uscrt
-  # secretd genesis add-genesis-account "$(secretd keys show -a b)" 1000000000000000000uscrt
-  # secretd genesis add-genesis-account "$(secretd keys show -a c)" 1000000000000000000uscrt
-  # secretd genesis add-genesis-account "$(secretd keys show -a d)" 1000000000000000000uscrt
-  secretd genesis add-genesis-account a 1000000000000000000uscrt
-  secretd genesis gentx a 1000000uscrt --chain-id "$chain_id"
-  # secretd gentx b 1000000uscrt --chain-id "$chain_id"
-  # secretd gentx c 1000000uscrt --chain-id "$chain_id"
-  # secretd gentx d 1000000uscrt --chain-id "$chain_id"
+  secretd keys list --output json | jq
+
+  ico=1000000000000000000
+
+  secretd genesis add-genesis-account validator ${ico}uscrt
+  secretd genesis add-genesis-account a ${ico}uscrt
+  secretd genesis add-genesis-account b ${ico}uscrt
+  secretd genesis add-genesis-account c ${ico}uscrt
+  secretd genesis add-genesis-account d ${ico}uscrt
+  
+  secretd genesis gentx validator ${ico}uscrt --chain-id "$chain_id"
 
   secretd genesis collect-gentxs
   secretd genesis validate-genesis
@@ -109,5 +113,4 @@ if [ "${SLEEP}" = "true" ]; then
   sleep infinity
 fi
 
-RUST_BACKTRACE=1 LOG_LEVEL="$LOG_LEVEL" secretd start --rpc.laddr tcp://0.0.0.0:26657 --bootstrap
-
+RUST_BACKTRACE=1 LOG_LEVEL="$LOG_LEVEL" secretd start --rpc.laddr tcp://0.0.0.0:26657 --bootstrap --log_level debug
