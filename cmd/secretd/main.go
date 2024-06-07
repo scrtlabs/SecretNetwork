86f43b52590be18ebef22b4af8728f94e06b4dba
Ilya Raykker <ilya.th.kapitonov@gmail.com>
2024-06-07 11:33:08 +0400

    Remove duplicated error message printing

--- cmd/secretd/main.go
+++ cmd/secretd/main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"github.com/scrtlabs/SecretNetwork/app"
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+	"github.com/scrtlabs/SecretNetwork/app"
+	"os"
 )
 
 func main() {
 	rootCmd, _ := NewRootCmd()
 	if err := svrcmd.Execute(rootCmd, "SECRET_NETWORK", app.DefaultNodeHome); err != nil {
-		fmt.Fprintln(rootCmd.OutOrStderr(), err)
 		os.Exit(1)
 	}
 }
