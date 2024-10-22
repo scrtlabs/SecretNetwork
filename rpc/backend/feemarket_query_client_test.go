6c4e2b560437b3fd0fd9d21e87a03ce8aecd2e48
cboh4 <bohdan@scrtlabs.com>
2024-10-22 13:01:33 +0300

    wip: added rpc folder

---
+++ rpc/backend/feemarket_query_client_test.go
@@ -0,0 +1,22 @@
+package backend
+
+import (
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
+	"github.com/scrtlabs/SecretNetwork/rpc/backend/mocks"
+	rpc "github.com/scrtlabs/SecretNetwork/rpc/types"
+	feemarkettypes "github.com/scrtlabs/SecretNetwork/x/feemarket/types"
+)
+
+var _ feemarkettypes.QueryClient = &mocks.FeeMarketQueryClient{}
+
+// Params
+func RegisterFeeMarketParams(feeMarketClient *mocks.FeeMarketQueryClient, height int64) {
+	feeMarketClient.On("Params", rpc.ContextWithHeight(height), &feemarkettypes.QueryParamsRequest{}).
+		Return(&feemarkettypes.QueryParamsResponse{Params: feemarkettypes.DefaultParams()}, nil)
+}
+
+func RegisterFeeMarketParamsError(feeMarketClient *mocks.FeeMarketQueryClient, height int64) {
+	feeMarketClient.On("Params", rpc.ContextWithHeight(height), &feemarkettypes.QueryParamsRequest{}).
+		Return(nil, sdkerrors.ErrInvalidRequest)
+}
