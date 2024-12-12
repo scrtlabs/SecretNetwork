d3771e0deae0a02df41cedd71d23b334e3ebf21e
Ilya Raykker <ilya.th.kapitonov@gmail.com>
2024-12-12 13:23:15 +0400

    Add debug output

--- deployment/ci/go-tests.sh
+++ deployment/ci/go-tests.sh
@@ -3,4 +3,7 @@ set -euv
 export LD_LIBRARY_PATH=/usr/lib:/usr/local/lib:/opt/sgxsdk/sdk_libs:/usr/lib/x86_64-linux-gnu/
 # Run go system tests for compute module
 mkdir -p ./x/compute/internal/keeper/.sgx_secrets
+go env
+go version
+go mod tidy
 LOG_LEVEL=ERROR GOMAXPROCS=8 SCRT_SGX_STORAGE='./' go test -tags "sgx hw test" -failfast -timeout 90m -v ./x/compute/internal/...
