b68e78021a8a2bdb3970d78c03eaea17e06aa65e
cboh4 <bohdan@scrtlabs.com>
2025-11-05 15:14:15 +0200

    fix

--- x/compute/internal/keeper/handler_plugin.go
+++ x/compute/internal/keeper/handler_plugin.go
@@ -610,21 +610,6 @@ func (h SDKMessageHandler) handleSdkMessage(ctx sdk.Context, contractAddr sdk.Ad
 	return nil, sdkerrors.ErrUnknownRequest.Wrapf("can't route message %+v", msg)
 }
 
-// callDepthMessageHandler is a wrapper around a Messenger that checks the call depth before dispatching a message.
-type callDepthMessageHandler struct {
-	Messenger
-	MaxCallDepth uint32
-}
-
-func (h callDepthMessageHandler) DispatchMsg(ctx sdk.Context, contractAddr sdk.AccAddress, contractIBCPortID string, msg v1wasmTypes.CosmosMsg) (events []sdk.Event, data [][]byte, err error) {
-	ctx, err = checkAndIncreaseCallDepth(ctx, h.MaxCallDepth)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	return h.Messenger.DispatchMsg(ctx, contractAddr, contractIBCPortID, msg)
-}
-
 // convertWasmIBCTimeoutHeightToCosmosHeight converts a wasm type ibc timeout height to ibc module type height
 func convertWasmIBCTimeoutHeightToCosmosHeight(ibcTimeoutBlock *v1wasmTypes.IBCTimeoutBlock) ibcclienttypes.Height {
 	if ibcTimeoutBlock == nil {
